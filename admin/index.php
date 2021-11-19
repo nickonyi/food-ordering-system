a65f8f0c36cd9fd51a838978e3850f91f76de812
nickonyi <nickoluoch1130@gmail.com>
2021-11-19 07:02:15 +0300

    first commit

---
+++ admin/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/admin.css">
+    <title>Food order system -home page</title>
+</head>
+<body>
+    <!--Menu section starts--->
+    <div class="menu">
+    <div class="wrapper">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Admin</a></li>
+        <li><a href="#"></a>Food</li>
+        <li><a href="#"></a>Order</li>
+        <li><a href="#"></a>Category</li>
+    </ul>
+    </div>  
+    </div>
+    <!--Menu section ends--->
+    <!--Main  content  Section starts--->
+    <div class="main-content">
+    <div class="wrapper">
+   
+    </div>    
+    
+    </div>
+    <!---Main content section ends--->
+    <!--Footer section starts--->
+    <div class="footer">
+    <div class="wrapper">
+    <p class="text-center">2021 all rights reserved wowFood,developed by <a href="#">Nickleyde Onyango</a></p>
+    </div>  
+    </div>
+    <!---Footer section ends--->
+</body>
+</html>
